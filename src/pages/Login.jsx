43d860617a8a786fff3e3d52b2435713777e376b
Soumen Dass <soumen.dass2024nst.rishihood.edu.in@Soumens-MacBook-Pro.local>
2025-06-27 20:17:40 +0530

    New image added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import planeImage from '../assets/image.png';
+import flyingPlane from '../assets/image copy.png';
+
 import './Login.css';
 
 function Login() {
@@ -19,13 +21,14 @@ function Login() {
         <div className="cloud cloud4"></div>
         <div className="cloud cloud5"></div>
         <div className="cloud cloud6"></div>
-        <div className="cloud cloud7"></div>
+        <div className="cloud cloud"></div>
 
         <img src={planeImage} alt="Plane" className="plane" />
+        <img src={flyingPlane} alt="Flying Plane" className="flying-plane" />
       </div>
 
       <div className="login-box">
-        <h2>Login to SkyWatch</h2>
+        <h2>Welcome back to SkyWatch</h2>
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Password" />
         <button onClick={handleLogin}>Login</button>
